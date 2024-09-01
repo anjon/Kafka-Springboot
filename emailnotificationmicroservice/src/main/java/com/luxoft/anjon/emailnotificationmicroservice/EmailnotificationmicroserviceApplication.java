5d6412995bedfc1c0f6d13f05f0af2d01ac67998
Anjon Sarker <anjon.sarker@gmail.com>
2024-09-01 13:52:51 +0000

    Created emailnotification kafka consumer

---
+++ emailnotificationmicroservice/src/main/java/com/luxoft/anjon/emailnotificationmicroservice/EmailnotificationmicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.luxoft.anjon.emailnotificationmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailnotificationmicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailnotificationmicroserviceApplication.class, args);
+	}
+
+}
---
+++ emailnotificationmicroservice/src/test/java/com/luxoft/anjon/emailnotificationmicroservice/EmailnotificationmicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luxoft.anjon.emailnotificationmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailnotificationmicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
