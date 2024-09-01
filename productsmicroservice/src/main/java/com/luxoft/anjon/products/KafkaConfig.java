28cc25faaf66c86d15700e59e1363d0e623d0ffb
Anjon Sarker <anjon.sarker@gmail.com>
2024-09-01 13:47:48 +0000

    Update application properties for idempotence

--- productsmicroservice/src/main/java/com/luxoft/anjon/products/KafkaConfig.java
+++ productsmicroservice/src/main/java/com/luxoft/anjon/products/KafkaConfig.java
@@ -38,7 +38,13 @@ public class KafkaConfig {
 
     @Value("${spring.kafka.producer.properties.request.timeout.ms}")
     private String requestTimeout;
-    
+
+    @Value("${spring.kafka.producer.properties.enable.idempotence}")
+    private String idempotence;
+
+    @Value("${spring.kafka.producer.properties.max.in.flight.requests.per.connection}")
+    private String inflightRequests;
+        
     Map<String, Object> producerConfigs() {
         Map<String, Object> config = new HashMap<>();
 
@@ -49,6 +55,8 @@ public class KafkaConfig {
         config.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, deliveryTimeout);
         config.put(ProducerConfig.LINGER_MS_CONFIG, linger);
         config.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, requestTimeout);
+        config.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, idempotence);
+        config.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, inflightRequests);
 
         return config;
     }
