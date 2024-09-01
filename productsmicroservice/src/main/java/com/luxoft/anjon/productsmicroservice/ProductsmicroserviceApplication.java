029b3167cdb5ce17410e2126ea11cd24d437cc44
Anjon Sarker <anjon.sarker@gmail.com>
2024-09-01 08:20:20 +0000

    Initialize springboot project

---
+++ productsmicroservice/src/main/java/com/luxoft/anjon/productsmicroservice/ProductsmicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.luxoft.anjon.productsmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsmicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsmicroserviceApplication.class, args);
+	}
+
+}
---
+++ productsmicroservice/src/test/java/com/luxoft/anjon/productsmicroservice/ProductsmicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luxoft.anjon.productsmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsmicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
