0845ed5a6b0db25025659fc32c5ef04eb6bc41c0
Anjon Sarker <anjon.sarker@gmail.com>
2024-09-01 09:05:46 +0000

    Created ProductID http response

---
+++ productsmicroservice/src/main/java/com/luxoft/anjon/products/service/ProductService.java
@@ -0,0 +1,8 @@
+package com.luxoft.anjon.products.service;
+
+import com.luxoft.anjon.products.rest.CreateProductRestModel;
+
+public interface ProductService {
+
+    String createProduct(CreateProductRestModel productRestModel);
+}
---
+++ productsmicroservice/src/main/java/com/luxoft/anjon/products/service/ProductServiceImpl.java
@@ -0,0 +1,15 @@
+package com.luxoft.anjon.products.service;
+
+import org.springframework.stereotype.Service;
+
+import com.luxoft.anjon.products.rest.CreateProductRestModel;
+
+@Service
+public class ProductServiceImpl implements ProductService{
+
+    @Override
+    public String createProduct(CreateProductRestModel productRestModel) {
+        return null;
+    }
+
+}
