f668c2164eb24f96dc922e77efb251020294d19f
Anjon Sarker <anjon.sarker@gmail.com>
2024-09-01 14:29:46 +0000

    Added core shared library for both producer and consumer

---
+++ core/src/test/java/com/luxoft/anjon/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luxoft.anjon.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
