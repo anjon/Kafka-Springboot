4b4d4adaf8087692535f4a8a42cab056ee60edc9
Anjon Sarker <anjon.sarker@gmail.com>
2024-09-02 10:15:10 +0000

    removed unused imports

--- emailnotificationmicroservice/src/main/java/com/luxoft/anjon/emailnotification/handler/ProductCreatedEventHandler.java
+++ emailnotificationmicroservice/src/main/java/com/luxoft/anjon/emailnotification/handler/ProductCreatedEventHandler.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.annotation.KafkaHandler;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.retry.RetryException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.ResourceAccessException;
