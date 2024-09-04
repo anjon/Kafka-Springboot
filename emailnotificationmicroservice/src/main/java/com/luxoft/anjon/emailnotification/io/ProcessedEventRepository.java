0a694f704b2c417422ffe5653ee7de6d046439fe
Anjon Sarker <anjon.sarker@gmail.com>
2024-09-04 15:46:32 +0000

    Added fix

--- emailnotificationmicroservice/src/main/java/com/luxoft/anjon/emailnotification/io/ProcessedEventRepository.java
+++ emailnotificationmicroservice/src/main/java/com/luxoft/anjon/emailnotification/io/ProcessedEventRepository.java
@@ -3,7 +3,9 @@ package com.luxoft.anjon.emailnotification.io;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface ProcessedEventRepository extends JpaRepository<ProcessedEventEntity, Long> {
 
+    ProcessedEventEntity findByMessageId(String messageId);
 }
