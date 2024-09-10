8713b47569f06fed63ec4c149fdf7e1a8dd0db95
Anjon Sarker <anjon.sarker@gmail.com>
2024-09-10 15:02:09 +0000

    Added rest controller in github actions

--- github-cicd-actions/src/main/java/com/intrum/anjon/GithubCicdActionsApplication.java
+++ github-cicd-actions/src/main/java/com/intrum/anjon/GithubCicdActionsApplication.java
@@ -2,9 +2,23 @@ package com.intrum.anjon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @SpringBootApplication
+@RestController
 public class GithubCicdActionsApplication {
+
+	@GetMapping("/welcome")
+	public String getMethodName(@RequestParam String param) {
+		return new String();
+	}
+	
+	public String Welcome() {
+		return "Welcome to SpringBoot -> Anjon";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(GithubCicdActionsApplication.class, args);
