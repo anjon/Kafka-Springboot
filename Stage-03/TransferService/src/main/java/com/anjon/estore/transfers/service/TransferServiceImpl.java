b99940b93a8dd2175c3ebecc4ac44d506b8287f8
Anjon Sarker <anjon.sarker@gmail.com>
2024-09-15 23:14:57 +0000

    update logging and TransferServiceImpl class to added jpa database transaction

--- Stage-03/TransferService/src/main/java/com/anjon/estore/transfers/service/TransferServiceImpl.java
+++ Stage-03/TransferService/src/main/java/com/anjon/estore/transfers/service/TransferServiceImpl.java
@@ -1,7 +1,10 @@
 package com.anjon.estore.transfers.service;
 
+import java.util.UUID;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -12,6 +15,8 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
 import com.anjon.estore.transfers.error.TransferServiceException;
+import com.anjon.estore.transfers.io.TransferEntity;
+import com.anjon.estore.transfers.io.TransferRepository;
 import com.anjon.estore.transfers.model.TransferRestModel;
 import com.appsdeveloperblog.payments.ws.core.events.DepositRequestedEvent;
 import com.appsdeveloperblog.payments.ws.core.events.WithdrawalRequestedEvent;
@@ -23,15 +28,17 @@ public class TransferServiceImpl implements TransferService {
 	private KafkaTemplate<String, Object> kafkaTemplate;
 	private Environment environment;
 	private RestTemplate restTemplate;
+	private TransferRepository transferRepository;
 
 	public TransferServiceImpl(KafkaTemplate<String, Object> kafkaTemplate, Environment environment,
-			RestTemplate restTemplate) {
+			RestTemplate restTemplate, TransferRepository transferRepository) {
 		this.kafkaTemplate = kafkaTemplate;
 		this.environment = environment;
 		this.restTemplate = restTemplate;
+		this.transferRepository = transferRepository;
 	}
 
-	@Transactional("kafkaTransactionManager")
+	@Transactional("transactionManager") 	// for kafka transaction use "kafkaTransactionManager"
 	@Override
 	public boolean transfer(TransferRestModel transferRestModel) {
 		WithdrawalRequestedEvent withdrawalEvent = new WithdrawalRequestedEvent(transferRestModel.getSenderId(),
@@ -39,7 +46,16 @@ public class TransferServiceImpl implements TransferService {
 		DepositRequestedEvent depositEvent = new DepositRequestedEvent(transferRestModel.getSenderId(),
 				transferRestModel.getRecepientId(), transferRestModel.getAmount());
 
+		TransferEntity transferEntity = new TransferEntity();
+		BeanUtils.copyProperties(transferRestModel, transferEntity);
+		transferEntity.setTransferId(UUID.randomUUID().toString());
+
 		try {
+
+			// Save records to a database table
+			
+			transferRepository.save(transferEntity);
+
 			kafkaTemplate.send(environment.getProperty("withdraw-money-topic", "withdraw-money-topic"),
 					withdrawalEvent);
 			LOGGER.info("Sent event to withdrawal topic.");
