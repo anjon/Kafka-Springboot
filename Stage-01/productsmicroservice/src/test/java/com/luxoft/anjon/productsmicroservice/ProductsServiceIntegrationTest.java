a6d3741340ce1e1f3a8a7cbe3710626a23072e4e
Anjon Sarker <anjon.sarker@gmail.com>
2024-09-16 00:09:58 +0000

    added integration tests

---
+++ Stage-01/productsmicroservice/src/test/java/com/luxoft/anjon/productsmicroservice/ProductsServiceIntegrationTest.java
@@ -0,0 +1,22 @@
+package com.luxoft.anjon.productsmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+
+@DirtiesContext
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ActiveProfiles("test")     // application-test.properties
+@EmbeddedKafka(partitions=3, count=3, controlledShutdown=true)
+@SpringBootTest(properties="spring.kafka.prosucer.bootstrap-servers=${spring.embedded.kafka.brokers}")
+public class ProductsServiceIntegrationTest {
+
+    @Test
+    void testCreateProduct_whenGivenvalidProductDetails_successfullSendsKafkaMessage() {
+
+    }
+    
+}
