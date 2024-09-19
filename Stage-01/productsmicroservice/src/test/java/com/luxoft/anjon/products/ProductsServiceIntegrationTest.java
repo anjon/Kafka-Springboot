31b1abd5389df90582e89ce2685cfa9c539516de
Anjon Sarker <anjon.sarker@gmail.com>
2024-09-19 12:52:10 +0000

    Update test class and application properties

--- Stage-01/productsmicroservice/src/test/java/com/luxoft/anjon/products/ProductsServiceIntegrationTest.java
+++ Stage-01/productsmicroservice/src/test/java/com/luxoft/anjon/products/ProductsServiceIntegrationTest.java
@@ -1,12 +1,25 @@
 package com.luxoft.anjon.products;
 
+import java.math.BigDecimal;
+import java.util.Map;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
+
+import com.luxoft.anjon.products.rest.CreateProductRestModel;
+import com.luxoft.anjon.products.service.ProductService;
 
 @DirtiesContext
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -16,15 +29,44 @@ import org.springframework.test.context.ContextConfiguration;
 @ContextConfiguration
 public class ProductsServiceIntegrationTest {
 
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private EmbeddedKafkaBroker embeddedKafkaBroker;
+
+    @Autowired
+    Environment environment;
+
     @Test
-    void testCreateProduct_whenGivenvalidProductDetails_successfullSendsKafkaMessage() {
+    void testCreateProduct_whenGivenvalidProductDetails_successfullSendsKafkaMessage() throws Exception{
         
         // Arrange
+        String title = "iPhone 16";
+        BigDecimal price = new BigDecimal(600);
+        Integer quantity = 1;
+
+        CreateProductRestModel createProductRestModel = new CreateProductRestModel();
+        createProductRestModel.setTitle(title);
+        createProductRestModel.setPrice(price);
+        createProductRestModel.setQuantity(quantity);
 
         // Act
+        productService.createProduct(createProductRestModel);
 
         // Assert
 
+    }
+
+    private Map<String, Object> getConsumerProperties() {
+        return Map.of(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, embeddedKafkaBroker.getBrokersAsString(),
+        ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class, 
+        ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class,
+        ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class,
+        ConsumerConfig.GROUP_ID_CONFIG, environment.getProperty("spring.kafka.consumer.group-id"),
+        JsonDeserializer.TRUSTED_PACKAGES, environment.getProperty("spring.kafka.consumer.properties.string.json.trusted.packages"),
+        ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, environment.getProperty("spring.kafka.consumer.auto-offset-reset")
+        );
     }
 
 }
